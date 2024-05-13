154b80059b1c5b1ea6cb783000520109e976c285
sweetSupremum <siroezhka29@yandex.ru>
2024-05-13 15:22:49 +0400

    slize points

--- src/main/java/tsvetkoff/mapper/GraphMapper.java
+++ src/main/java/tsvetkoff/mapper/GraphMapper.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import tsvetkoff.domain.Graph;
 import tsvetkoff.domain.GraphDto;
+import tsvetkoff.utill.StepUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,16 +23,25 @@ public class GraphMapper {
      * (сначала проходим в цикле по нулевому потом уже добавляем это количество точек)
      * т.е. на 20 точек будет 20+1 (нулевая точка). Если же количество точек будет меньше чем pointCount*2, то выведем все
      * pointCount * 2
-     */ //TODO:
+     */
     private int pointCount;
 
 
     public GraphDto mapLastCallsGraphsToDto(Graph graph) {
-        return GraphDto.builder().r(graph.getR()).t(graph.getTimes()).nameLabelOrdinatesMap(graph.getOnlyLastCallGraphs()).build();
+        return GraphDto
+                .builder()
+                .r(StepUtils.getArrayWithSlize(graph.getR(), pointCount))
+                .t(StepUtils.getArrayWithSlize(graph.getTimes(), pointCount))
+                .nameLabelOrdinatesMap(getGraphWithSlize(graph.getOnlyLastCallGraphs()))
+                .build();
     }
 
     public GraphDto mapTempCallsGraphsToDto(Graph graph) {
-        return GraphDto.builder().r(graph.getR()).nameLabelOrdinatesMap(graph.getTwoDimensionalGraphWithName()).build();
+        return GraphDto
+                .builder()
+                .r(StepUtils.getArrayWithSlize(graph.getR(), pointCount))
+                .nameLabelOrdinatesMap(getGraphWithSlize(graph.getTwoDimensionalGraphWithName()))
+                .build();
     }
 
     public GraphDto getAll(Graph graph) {
@@ -39,6 +49,25 @@ public class GraphMapper {
         GraphDto tempCallsGraphs = mapTempCallsGraphsToDto(graph);
         Map<String, Map<String, double[]>> allGraphs = new HashMap<>(tempCallsGraphs.getNameLabelOrdinatesMap());
         allGraphs.putAll(lastCallGraphs.getNameLabelOrdinatesMap());
-        return GraphDto.builder().r(lastCallGraphs.getR()).t(lastCallGraphs.getT()).nameLabelOrdinatesMap(allGraphs).build();
+        return GraphDto.builder()
+                .r(StepUtils.getArrayWithSlize(lastCallGraphs.getR(), pointCount))
+                .t(StepUtils.getArrayWithSlize(lastCallGraphs.getT(), pointCount))
+                .nameLabelOrdinatesMap(allGraphs)
+                .build();
+    }
+
+    public <T> Map<String, Map<String, double[]>> getGraphWithSlize(Map<String, Map<String, double[]>> graph) {
+        Map<String, Map<String, double[]>> graphWithSlizes = new HashMap<>();
+        for (Map.Entry<String, Map<String, double[]>> graphEntry : graph.entrySet()) {
+            Map<String, double[]> slizeGraph = new HashMap<>();
+            String key = graphEntry.getKey();
+            Map<String, double[]> coordinatesMap = graphEntry.getValue();
+            for (Map.Entry<String, double[]> coordinatesEntry : coordinatesMap.entrySet()) {
+                double[] slizeCoordinates = StepUtils.getArrayWithSlize(coordinatesEntry.getValue(), pointCount);
+                slizeGraph.put(coordinatesEntry.getKey(), slizeCoordinates);
+            }
+            graphWithSlizes.put(key, slizeGraph);
+        }
+        return graphWithSlizes;
     }
 }
--- src/main/java/tsvetkoff/utill/StepUtils.java
+++ src/main/java/tsvetkoff/utill/StepUtils.java
@@ -2,6 +2,9 @@ package tsvetkoff.utill;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author SweetSupremum
  */
@@ -13,5 +16,29 @@ public class StepUtils {
             return 1.;
         }
         return ((double) (streamLength - 1) / (double) pointCount);
+    }
+
+    @SuppressWarnings("unchecked")
+    public double[] getArrayWithSlize(double[] list, int pointCount) {
+        int length = list.length;
+        double step = getStep(length, pointCount);
+        double[] slizeList = new double[pointCount + 1];
+        int counter = 0;
+        for (double i = 0; i < length; i += step) {
+            slizeList[counter] = list[(int) i];
+            counter++;
+        }
+        return slizeList;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Double> getArrayWithSlize(List<Double> list, int pointCount) {
+        int length = list.size();
+        double step = getStep(length, pointCount);
+        List<Double> slizeList = new ArrayList<>();
+        for (double i = 0; i < length; i += step) {
+            slizeList.add(list.get((int) i));
+        }
+        return slizeList;
     }
 }
